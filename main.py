f3ba4221c001a69859bbe6c8b3c183f1814dc8bb
TychoElling <tycho.elling@gmail.com>
2023-04-18 20:07:46 -0700

    asdf

--- main.py
+++ main.py
@@ -1,7 +1,36 @@
-import thing
+
 # Press the green button in the gutter to run the script.
+
+
+f = open("EX.txt","w")
+f.write("P3\n")
+f.write("256 256\n")
+c = (256,0,0)
+for i in range(256):
+    s = ""
+    c1=tuple((list(c)).copy())
+    for j in range(256):
+        c1 = (max(0,c1[0]-1),min(c1[1]+1,255),c1[2])
+        s += fff(c1[0])+ " "+fff(c1[1])+ " "+ fff(c1[2]) + "  "
+
+    f.write(s+"\n")
+    c = (max(0, c[0] - 1), c[1],min(c[2] + 1, 255))
+
+
+def writeColor(r,g,b):
+    def fff(a):
+        a = str(a)
+        if len(a) == 2:
+            return " " + a
+        if len(a) == 3:
+            return a
+        return "  " + a
+    r,g,b = str(r),str(g),str(b)
+    rgb = fff(r)+" " + fff(g) + " " + fff(b)
+    f = open("EX.txt","w")
+    f.write(rgb,"  ")
+
 if __name__ == '__main__':
-    print(thing.message)
+    pass
 
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
-
---
+++ vec3.py
@@ -0,0 +1,37 @@
+import math
+class vec3:
+    def __init__ (self,e1,e2,e3):
+        self.e1 = e1
+        self.e2 = e2
+        self.e3 = e3
+    def __add__(self,v1):
+        self.e1 += v1.e1
+        self.e2 += v1.e2
+        self.e3 += v1.e3
+        return self
+    def neg (self):
+        self.e1 = -self.e1
+        self.e2 = -self.e2
+        self.e3 = -self.e3
+    def mult(self,t)
+        self.e1 *= t
+        self.e2 *= t
+        self.e3 *= t
+    def div(self,t):
+        self.mult(1/t)
+    def len(self):
+        return math.sqrt(self.e1**2 + self.e2**2 + self.e3**2)
+    def print(self):
+        print(self.e1,self.e2,self.e3)
+    def dot(self,v):
+        v1 = vec3(self.e1*v.e1,self.e2*v.e2,self.e3*v.e3)
+        return v1
+    def __mul__(self,t):
+        v1 = vec3(self.e1 * t, self.e2 * t, self.e3 * t)
+        return v1
+def cross(v,u):
+    return vec3(
+        u.e2*v.e3 - u.e3*v.e2,
+        u.e3*v.e1 - u.e1-v.e3,
+        u.e1*v.e2 - u.e2*v.e1
+    )
