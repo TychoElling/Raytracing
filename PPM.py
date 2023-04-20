4651ef74624dd81312648593d974bbf6f673dff5
TychoElling <69398745+TychoElling@users.noreply.github.com>
2023-04-20 14:03:25 -0700

    Update PPM.py
    Errors

--- PPM.py
+++ PPM.py
@@ -2,9 +2,9 @@ import vec3
 
 
 class PPM:
-    def __init__(self):
+    def __init__(self,x,y):
         self.f = open("EX.txt","w")
-        self.f.write("P3\n" + str(x) + " " + str(y), "\n")
+        self.f.write("P3\n" + str(x) + " " + str(y)+ "\n")
 
     def writeColor(self, rgb):
         def fff(a):
@@ -17,17 +17,13 @@ class PPM:
 
         r, g, b = str(rgb.e1), str(rgb.e2), str(rgb.e3)
         rgb = fff(r) + " " + fff(g) + " " + fff(b)
-        self.f.write(rgb, "  ")
+        self.f.write(rgb + "  ")
 
     def writeFile(self,grid, x, y):
        def writeLine(lst):
             for rgb in lst:
                 writeColor(rgb)
             self.f.write("\n")
-        for lst in grid:
+       for lst in grid:
             writLine(lst)
 
-
-
-
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
