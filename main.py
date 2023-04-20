d13e1fc83561fd5d1a49a341a70c843fb7167fd1
TychoElling <69398745+TychoElling@users.noreply.github.com>
2023-04-20 14:02:46 -0700

    Update main.py
    Errors

--- main.py
+++ main.py
@@ -16,22 +16,22 @@ viewportHeight = 2.0
 viewportWidth = 2 * aspectRatio
 focalLength = 1.0
 
-origin = vec3(0,0,0)
-horizontal = vec3(viewportWidth,0,0)
-vertical = vec3(0,viewportHeight,0)
-lowerLeftCorner = origin - horizontal/2 - vertical/2 - vec3(0,0,focalLength)
+origin = vec3.vec3(0,0,0)
+horizontal = vec3.vec3(viewportWidth,0,0)
+vertical = vec3.vec3(0,viewportHeight,0)
+lowerLeftCorner = origin - horizontal/2 - vertical/2 - vec3.vec3(0,0,focalLength)
 
 
 ### Render
 
-file = PPM()
+file = PPM.PPM(imageWidth,imageHeight)
 for j in range(imageHeight - 1, -1, -1):
   for i in range(0,imageWidth):
     u = i / (imageWidth - 1)
     v = j / (imageHeight - 1)
-    w = lowerLeftCorner + u*horizontal + v*vertical - origin
-    r = ray(origin.e1, origin.e2, origin.e3, w.e1, w.e2, w.e3)
-    pixelColor = vec3(int(j*256/imageHeight),0,0)
+    w = lowerLeftCorner + horizontal*u + vertical*v - origin
+    r = ray.ray(origin.e1, origin.e2, origin.e3, w.e1, w.e2, w.e3)
+    pixelColor = vec3.vec3(int(j*256/imageHeight),0,0)
     file.writeColor(pixelColor)
-  print("Line "+str(i+1)+" is finished")
+  print("Line "+str(j+1)+" is finished")
 print("Finished")
